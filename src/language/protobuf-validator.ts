e8f4faee72576186e2bb5e36559895345b631f25
Lachance, Alexandre <alexandrelachance@me.com>
2024-05-08 13:35:26 -0400

    Minimal example.

--- src/language/protobuf-validator.ts
+++ src/language/protobuf-validator.ts
@@ -1,17 +1,17 @@
-import type { ValidationAcceptor, ValidationChecks } from 'langium';
-import type { ProtobufAstType, Person } from './generated/ast.js';
+// import type { ValidationAcceptor, ValidationChecks } from 'langium';
+// import type { ProtobufAstType, Person } from './generated/ast.js';
 import type { ProtobufServices } from './protobuf-module.js';
 
 /**
  * Register custom validation checks.
  */
 export function registerValidationChecks(services: ProtobufServices) {
-    const registry = services.validation.ValidationRegistry;
-    const validator = services.validation.ProtobufValidator;
-    const checks: ValidationChecks<ProtobufAstType> = {
-        Person: validator.checkPersonStartsWithCapital
-    };
-    registry.register(checks, validator);
+    // const registry = services.validation.ValidationRegistry;
+    // const validator = services.validation.ProtobufValidator;
+    // const checks: ValidationChecks<ProtobufAstType> = {
+    //     Person: validator.checkPersonStartsWithCapital
+    // };
+    // registry.register(checks, validator);
 }
 
 /**
@@ -19,13 +19,13 @@ export function registerValidationChecks(services: ProtobufServices) {
  */
 export class ProtobufValidator {
 
-    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
-        if (person.name) {
-            const firstChar = person.name.substring(0, 1);
-            if (firstChar.toUpperCase() !== firstChar) {
-                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
-            }
-        }
-    }
+    // checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
+    //     if (person.name) {
+    //         const firstChar = person.name.substring(0, 1);
+    //         if (firstChar.toUpperCase() !== firstChar) {
+    //             accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+    //         }
+    //     }
+    // }
 
 }
