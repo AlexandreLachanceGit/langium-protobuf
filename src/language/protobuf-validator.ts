25d213efdc4920955784d694587c74201fd59e03
Lachance, Alexandre <alexandrelachance@me.com>
2024-05-14 15:21:36 -0400

    Add example validator.

--- src/language/protobuf-validator.ts
+++ src/language/protobuf-validator.ts
@@ -1,31 +1,28 @@
-// import type { ValidationAcceptor, ValidationChecks } from 'langium';
-// import type { ProtobufAstType, Person } from './generated/ast.js';
+import type { ValidationAcceptor, ValidationChecks } from 'langium';
+import { type ProtobufAstType, type EnumField, EnumBody } from './generated/ast.js';
 import type { ProtobufServices } from './protobuf-module.js';
 
 /**
  * Register custom validation checks.
  */
 export function registerValidationChecks(services: ProtobufServices) {
-    // const registry = services.validation.ValidationRegistry;
-    // const validator = services.validation.ProtobufValidator;
-    // const checks: ValidationChecks<ProtobufAstType> = {
-    //     Person: validator.checkPersonStartsWithCapital
-    // };
-    // registry.register(checks, validator);
+    const registry = services.validation.ValidationRegistry;
+    const validator = services.validation.ProtobufValidator;
+    const checks: ValidationChecks<ProtobufAstType> = {
+        EnumField: validator.checkEnumFieldIdIsAllCaps
+    };
+    registry.register(checks, validator);
 }
 
 /**
  * Implementation of custom validations.
  */
 export class ProtobufValidator {
-
-    // checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
-    //     if (person.name) {
-    //         const firstChar = person.name.substring(0, 1);
-    //         if (firstChar.toUpperCase() !== firstChar) {
-    //             accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
-    //         }
-    //     }
-    // }
-
+    checkEnumFieldIdIsAllCaps(enumField: EnumField, accept: ValidationAcceptor): void {
+        if (enumField.name && enumField.name != enumField.name.toUpperCase()) {
+            if (enumField.$container?.$type == EnumBody) { // necessary because of bug?
+                accept('warning', 'Enum field identifier should be ALL CAPS.',  {node: enumField, property: 'name'})
+            }
+        }
+    }
 }
